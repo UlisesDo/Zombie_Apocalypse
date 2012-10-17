aec95113cd3a0b2373646df99459bbab0262e734
Ulises Dominguez <ulises_dguez_brito@yahoo.es>
2012-10-17 21:10:39 +0200

    Initial commit. Repository just created
    Initial Readme, gemfile and database config file have been created.

---
+++ zombie_apocalypse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieApocalypse::Application.config.secret_token = 'ce2fad2f3c321f9d234ea574038d2e19de2f24803bb3256b29d9c6d669393725dd24d5de4de1f5eaa5eda28c1231c174fb40f69eb8eae9164a977b4f1106e35a'
